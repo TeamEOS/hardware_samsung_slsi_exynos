36d0a21cdce6efe467faf208c123789c93640b53
Ethan Chen <intervigil@gmail.com>
2013-10-24 16:11:51 -0700

    exynos: Add kernel headers to include path
    Change-Id: If0afb51b7f9c63a3f8ccf96caf83bb9e47eec2a8

--- libcsc/Android.mk
+++ libcsc/Android.mk
@@ -10,9 +10,13 @@ LOCAL_SRC_FILES := \
 	csc.c
 
 LOCAL_C_INCLUDES := \
+	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
 	hardware/samsung_slsi/$(PLATFORM_DIR)/include \
 	$(LOCAL_PATH)/../include \
 	$(LOCAL_PATH)/../libexynosutils
+
+LOCAL_ADDITIONAL_DEPENDENCIES := \
+	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
 
